6448a39220113403cf764f2dafefc86b205c3c96
vitalya <vitalyatrololo@mail.ru>
2021-06-25 13:29:19 +0300

    Add unit test for flux on error return

--- src/test/java/com/reactorcourse/demo/FluxErrorHandlingTest.java
+++ src/test/java/com/reactorcourse/demo/FluxErrorHandlingTest.java
@@ -9,16 +9,30 @@ public class FluxErrorHandlingTest {
     public void itShouldHandleError() {
         Flux<String> testFlux = Flux.just("Some", "text")
                 .concatWith(Flux.error(new RuntimeException("Error")))
-                .log()
                 .onErrorResume(e -> {
                     System.out.println(e);
-                    return Flux.just("biba!").log();
+                    return Flux.just("biba!");
                 });
 
-        StepVerifier.create(testFlux)
+        StepVerifier.create(testFlux.log())
                 .expectNext("Some")
                 .expectNext("text")
                 .expectNext("biba!")
+                .verifyComplete();
+    }
+
+    @Test
+    public void itShouldResumeWithDefaultOnError() {
+        Flux<String> testFlux = Flux.just("some", "text")
+                .concatWith(Flux.error(new RuntimeException("error")))
+                .concatWith(Flux.just("never accessed"))
+                .onErrorReturn("default");
+
+        StepVerifier.create(testFlux.log())
+                .expectSubscription()
+                .expectNext("some")
+                .expectNext("text")
+                .expectNext("default")
                 .verifyComplete();
     }
 }
