3d4a90666ab3e3b14345123f30e1d18692eaf8ee
vitalya <vitalyatrololo@mail.ru>
2021-06-26 21:58:29 +0300

    Add simple reactive controller Add infinite streaming endpoint Add unit tests

--- src/main/java/com/reactorcourse/demo/contoller/ReactiveController.java
+++ src/main/java/com/reactorcourse/demo/contoller/ReactiveController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -18,10 +19,21 @@ public class ReactiveController {
                 .log();
     }
 
-    @GetMapping(value = "elements/stream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping("element")
+    public Mono<Integer> getElement() {
+        return Mono.just(666).log();
+    }
+
+    @GetMapping(value = "elements/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Integer> getElementsStream() {
         return Flux.just(1, 2, 3, 4)
                 .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+    @GetMapping(value = "elements/stream/infinite", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> getElementsStreamInfinite() {
+        return Flux.interval(Duration.ofSeconds(1))
                 .log();
     }
 }
---
+++ src/test/java/com/reactorcourse/demo/controller/ReactiveControllerTest.java
@@ -0,0 +1,66 @@
+package com.reactorcourse.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebFluxTest
+public class ReactiveControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void itShouldReturnFluxResponse() {
+        VirtualTimeScheduler.getOrSet();
+
+        Flux<Integer> responseBody = webTestClient.get()
+                .uri("/elements/stream")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.withVirtualTime(() -> responseBody)
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(4))
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void itShouldPublishInfiniteElements() {
+        Flux<Long> responseBody = webTestClient.get().uri("/elements/stream/infinite")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L)
+                .thenCancel()
+                .verify();
+    }
+
+    @Test
+    public void itShouldPublishOneElement() {
+        webTestClient.get().uri("/element")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(response -> assertEquals(666, response.getResponseBody()));
+    }
+}
